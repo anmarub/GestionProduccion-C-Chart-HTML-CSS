0a2209b820581a9ec627524efc01fa494cc5500f
anmarub <76189850+anmarub@users.noreply.github.com>
2021-10-03 18:51:32 -0500

    Actualizacion de Capa Dominio y Persistencia

---
+++ Produccion.App/Produccion.App.Persistence/AppRepository/IRepositoryOcDetailsFkProduct.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Produccion.App.Domain.Entities;
+
+namespace Produccion.App.Persistence.AppRepository
+{
+    public interface IRepositoryOcDetailsFkProduct
+    {
+        
+         
+    }
+}
